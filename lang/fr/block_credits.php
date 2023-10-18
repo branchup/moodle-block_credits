b803a444515662030bf2e24f9714da21f548d87c
Frédéric Massart <fred@branchup.tech>
2023-10-18 15:38:48 +0800

    Publish the French language file

---
+++ lang/fr/block_credits.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of a 3rd party created module for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package    block_credits
+ * @copyright  2023 Institut français du Japon
+ * @author     Frédéric Massart <fred@branchup.tech>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['actinguserid'] = 'ID utilisateur actif';
+$string['addcredits'] = 'Ajouter des crédits';
+$string['alreadyexipired'] = 'Expiré';
+$string['amount'] = 'Montant';
+$string['available'] = 'Disponible';
+$string['balance'] = 'Silde';
+$string['backtocourse'] = 'Retour au cours';
+$string['backtodashboard'] = 'Retour au tableau de bord';
+$string['cannotaudituser'] = 'Utilisateur non consultable';
+$string['cannotmanageuser'] = 'Utilisateur non modifiable';
+$string['creditedon'] = 'Crédit ajouté le';
+$string['credits:addinstance'] = 'Ajouter un bloc';
+$string['credits:manage'] = 'Gérer tous les crédits';
+$string['credits:myaddinstance'] = 'Ajouter le bloc à mon tableau de bord';
+$string['credits:view'] = 'Voir les crédits';
+$string['credits:viewall'] = 'Voir tous les crédits';
+$string['credits'] = 'Crédits';
+$string['creditshistory'] = 'Historique des crédits';
+$string['downloadtxs'] = 'Télécharger l\'historique des transactions';
+$string['expired'] = 'Expiré';
+$string['expirenow'] = 'Mettre à expiration';
+$string['expiringsoon'] = 'Expiration prochaine';
+$string['extendvalidity'] = 'Etendre la validité';
+$string['history'] = 'Historique';
+$string['label'] = 'Label';
+$string['manage'] = 'Gérer les crédits';
+$string['mycredits'] = 'Mes crédits';
+$string['ncredits'] = '{$a} crédit(s)';
+$string['note'] = 'Note';
+$string['nocreditstoexpire'] = 'Pas assez de crédits ({$a->available}/{$a->required}).';
+$string['nocreditsavailableatm'] = 'Vous n\'avez pas assez de crédits actuellement.';
+$string['notenoughtcredits'] = 'Pas assez de crédits ({$a->available}/{$a->required}).';
+$string['other'] = 'Autre';
+$string['pastcredits'] = 'Crédits passés';
+$string['pluginname'] = 'Crédits';
+$string['privatenote'] = 'Note privée';
+$string['publicnote'] = 'Note publique';
+$string['purchase'] = 'Achat';
+$string['reason'] = 'Motif';
+$string['reasonexpired'] = 'Crédit(s) expiré(s).';
+$string['reasonextended'] = 'Changement de date d\'expiration de crédits : {$a->from} > {$a->to}.';
+$string['reasonother'] = 'Mise à jour de crédit';
+$string['reasonpurchase'] = 'Achat de crédits, valides jusqu\' {$a->validuntil}.';
+$string['reasonrefundafterexpiry'] = 'Crédits rendus après expiration.';
+$string['reasonrefunded'] = 'Crédits annulés';
+$string['recordedon'] = 'Enregistré le';
+$string['refund'] = 'Annulation';
+$string['removefilter'] = 'Désactiver le filtre';
+$string['soonestexpiry'] = 'Date d\'expiration la plus proche';
+$string['systemuser'] = 'Système';
+$string['taskexpirecredits'] = 'Procéder à l\'expiration des crédits.';
+$string['total'] = 'Total';
+$string['transactions'] = 'Opérations';
+$string['transactionsfilteredforid'] = 'Liste des opérations pour le paquet de crédits n°{$a}';
+$string['used'] = 'Consommé';
+$string['usernotenrolledincurrentcourse'] = 'Cet utilisateur n\'est pas inscrit dans le cours.';
+$string['userscredits'] = 'Crédits de l\'utilisateur';
+$string['validuntil'] = 'Date d\'expiration';
+$string['viewall'] = 'Voir les crédits';
