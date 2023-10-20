679bc0c53efd9ce12c4769812f0117e121d326c4
Frédéric Massart <fred@branchup.tech>
2023-10-20 18:44:37 +0800

    Bump to v1.1.0

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version      = 2023102001;
+$plugin->version      = 2023102002;
 $plugin->requires     = 2022112800;   // Moodle 4.1.0.
 $plugin->component    = 'block_credits';
 $plugin->maturity     = MATURITY_STABLE;
-$plugin->release      = '1.0.4';
+$plugin->release      = '1.1.0';
 $plugin->dependencies = [];
