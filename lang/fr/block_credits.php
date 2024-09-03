57eeef01bfc96cf11d90a9c8c8effedc11b04ccd
Frédéric Massart <fred@branchup.tech>
2024-09-03 15:28:33 +0800

    Updating French and Japanese translations

--- lang/fr/block_credits.php
+++ lang/fr/block_credits.php
@@ -28,6 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 $string['actinguserid'] = 'ID utilisateur actif';
 $string['addcredits'] = 'Ajouter des crédits';
 $string['adjustquantity'] = 'Modifier la quantité';
+$string['alreadyexpired'] = 'Déjà expiré';
 $string['amount'] = 'Quantité';
 $string['available'] = 'Disponible';
 $string['backtocourse'] = 'Retour au cours';
@@ -41,9 +42,11 @@ $string['credits'] = 'Crédits';
 $string['credits:addinstance'] = 'Ajouter un bloc';
 $string['credits:manage'] = 'Gérer tous les crédits';
 $string['credits:myaddinstance'] = 'Ajouter le bloc à mon tableau de bord';
+$string['credits:receivemanagernotifications'] = 'Recevoir les notifications de manager.';
 $string['credits:view'] = 'Voir les crédits';
 $string['credits:viewall'] = 'Voir tous les crédits';
 $string['creditshistory'] = 'Historique des crédits';
+$string['csvfieldseparator'] = 'Séparateur de champs du CSV';
 $string['csvfile'] = 'Fichier CSV';
 $string['csvfile_help'] = 'Télécharger un fichier CSV containant les colonnes suivantes :
 
@@ -52,7 +55,6 @@ $string['csvfile_help'] = 'Télécharger un fichier CSV containant les colonnes 
 - validuntil: (requis) Date de validité des crédits en format YYYY-MM-DD
 - publicnote: (optionnel) Un commentaire public
 - privatenote: (optionnel) Un commentaire privé';
-$string['csvfieldseparator'] = 'Séparateur de champs du CSV';
 $string['downloadtxs'] = 'Télécharger l\'historique des opérations';
 $string['expired'] = 'Expiré';
 $string['expirenow'] = 'Mettre à expiration';
@@ -65,6 +67,29 @@ $string['importlinenskippedmissing'] = 'Ligne {$a} sautée : utilisateur invalid
 $string['importresults'] = 'Résultats de l\'import';
 $string['label'] = 'Commentaire';
 $string['manage'] = 'Gérer les crédits';
+$string['messageexpirednotice'] = 'Nous vous informons que {$a->credits} de vos crédits ont expiré.
+
+Veuilez consulter [la plateforme d\'apprentissage]({$a->wwwroot}) pour voir le détail de vos crédits.';
+$string['messageexpirednoticesubject'] = '{$a->credits} credits ont expiré.';
+$string['messageexpiredrefund'] = 'Vous recevez cet email car certains crédits n\'ont pas pu être remboursés, ou ont été remboursés mais expirent bientôt.
+
+- Utilisateur : {$a->fullname}
+- ID de l\'opération : {$a->opid}
+- Raison du remboursement : {$a->reason}
+- Quantité remboursée (expiration prochaine) : {$a->expiringsoon}
+- Quantité non remboursée (expirée) : {$a->expired}
+
+Pour gérer les crédits de cette personne, veuillez accéder à [cette page]({$a->url}).';
+$string['messageexpiredrefundsubject'] = 'Crédits expirés (ou en cours d\'expiration) après restitution';
+$string['messageexpirynotice'] = 'Cet email est un rappel pour vous informer que certains de vos crédits vont prochainement arriver à expiration.
+
+- Crédits : {$a->credits}
+- Date d\'expiration : {$a->expirydate}
+
+Veuillez utiliser vos crédits avant leur expiration pour éviter toute perte. Vous pouvez le faire sur [votre plateforme d\'apprentissage]({$a->wwwroot}).';
+$string['messageexpirynoticesubject'] = '{$a->credits} crédits expirent dans moins de {$a->days} jours.';
+$string['messageprovider:expiredrefund'] = 'Crédits expirés ou en cours d\'expiration qui sont restitués.';
+$string['messageprovider:expirynotice'] = 'Notifications d\'expiration';
 $string['mycredits'] = 'Mes crédits';
 $string['myongoingcredits'] = 'Mes crédits en cours';
 $string['ncredits'] = '{$a} crédit(s)';
@@ -82,6 +107,7 @@ $string['publicnote'] = 'Note publique';
 $string['purchase'] = 'Achat';
 $string['reason'] = 'Motif';
 $string['reasonexpired'] = 'Crédit(s) expiré(s).';
+$string['reasonexpirynoticesent'] = 'Notification d\'expiration prochaine envoyée ({$a->credits} crédits le {$a->expiry}).';
 $string['reasonextended'] = 'Changement de date d\'expiration de crédits : {$a->from} > {$a->to}.';
 $string['reasonimported'] = 'Crédits importés par l\'administrateur.';
 $string['reasonother'] = 'Mise à jour de crédit';
@@ -96,12 +122,15 @@ $string['removefilter'] = 'Désactiver le filtre';
 $string['soonestexpiry'] = 'Date d\'expiration la plus proche';
 $string['systemuser'] = 'Système';
 $string['taskexpirecredits'] = 'Procéder à l\'expiration des crédits.';
+$string['tasksendexpirynotices'] = 'Envoyer les notifications d\'expiration';
 $string['total'] = 'Total';
 $string['transactions'] = 'Opérations';
+$string['transactionsfiltered'] = 'Les transactions sont filtrées.';
 $string['transactionsfilteredforid'] = 'Liste des opérations pour le paquet de crédits n°{$a}';
 $string['used'] = 'Utilisé';
 $string['usernotenrolledincurrentcourse'] = 'Cet utilisateur n\'est pas inscrit dans le cours.';
 $string['userscredits'] = 'Etat des crédits de l\'utilisateur';
 $string['validuntil'] = 'Date d\'expiration';
+$string['validuntilchangenote'] = 'Veuillez noter que modifier la validité réinitialisera les notifications d\'expiration déjà envoyées.';
 $string['valuecannotbelessthan'] = 'Cette valeur ne peut pas être inférieure à {$a}.';
 $string['viewall'] = 'Voir les crédits';
